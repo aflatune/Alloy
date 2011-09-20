cd639d64f531126436a983382d81e331f2c2527c
Amol Kelkar <amolk@aflatune.com>
2011-09-19 23:50:18 -0700

    Split out sendRequest so it can be overriddet to send POST body, etc

--- view_model.js
+++ view_model.js
@@ -89,10 +89,10 @@ Alloy.ViewModel = new JS.Class({
       if (!params.async)
         Ti.App.fireEvent('app:hide.loader');
 
-      if(this.status != 200) {
-        this.onerror();
-        return;
-      }
+      //if(this.status >= 400) {
+      //  this.onerror();
+      //  return;
+      //}
       _this.currentRequest = null;
       
       info("xhr Call returned");
@@ -124,10 +124,14 @@ Alloy.ViewModel = new JS.Class({
     
     if (!params.async)
       Ti.App.fireEvent('app:show.loader');
-      
-    xhr.send();
+    
+    this.sendRequest(xhr, params);
   },
 
+  sendRequest: function(xhr, params) {
+    xhr.send();
+  },
+  
   parseData: function(response) {
     return eval('('+response.responseText+')');
   },
