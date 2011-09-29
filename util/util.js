d383d299140c4b26eab94f22d0828761b938f792
Amol Kelkar <amolk@aflatune.com>
2011-09-29 14:35:29 -0700

    Hash utilities

--- util/util.js
+++ util/util.js
@@ -1,2 +1,24 @@
 Util = {};
 Ti.include('string.js');
+
+Util.Hash = {
+  keys: function(o){
+     var ret=[],p;
+     for(p in o) ret.push(p);
+     return ret;
+  },
+  
+  values: function(o){
+     var ret=[],p;
+     for(p in o) ret.push(o[p]);
+     return ret;
+  },
+  
+  transpose: function(o) {
+     var ret={},p;
+     for(p in o) ret[o[p]] = p;
+     return ret;
+  }
+  
+};
+
