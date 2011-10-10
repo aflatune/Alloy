9f28747d9f880d331c4b704213f576a1ad204ee7
Amol Kelkar <amolk@aflatune.com>
2011-10-10 13:05:54 -0700

    Add date js

--- alloy.js
+++ alloy.js
@@ -7,6 +7,7 @@ if (typeof(AlloyConfig) == 'undefined') AlloyConfig = {};
 // Include external libraries
 include('extern/js_class_core.js');
 include('extern/md5.js');
+include('extern/date.js');
 
 // Include utilities
 include('util/util.js');
