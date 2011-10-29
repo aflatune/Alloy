cc16201743c1386d59b12a376781423ff1224df5
Amol Kelkar <amolk@aflatune.com>
2011-10-28 17:44:51 -0700

    Simple image gallery view

---
+++ ui/image_gallery_view.js
@@ -0,0 +1,34 @@
+var used = [Ti.UI.createScrollableView];
+
+Alloy.UI.ImageGalleryView = new JS.Class(Alloy.View, {
+  initialize: function() {
+    this.callSuper(true /* always a partial view */);
+  },
+
+  render: function(images, selected) {
+    var views = [];
+    for (var i in images) {
+      var image = images[i];
+      var container = new ScrollView({maxZoomScale: 3})
+      var imageView = new ImageView({image: image.photo_url, maxZoomScale: 3});
+      container.add(imageView);
+      views.push(container);
+    }
+    
+    var scrollableView = new ScrollableView({
+      views: views,
+      showPagingControl:true,
+      top: 0,
+      left: 0,
+      bottom: 0,
+      right: 0
+    });
+    
+    info("SELECTING " + selected);
+    setTimeout(function() {
+      scrollableView.scrollToView(selected);  
+    }, 100);
+    
+    this.view.add(scrollableView);
+  }
+})
