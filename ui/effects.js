853be7ce65857694de353b7448be99b639b65bab
Amol Kelkar <amolk@aflatune.com>
2011-11-06 00:16:51 -0700

    activate and nod effects

--- ui/effects.js
+++ ui/effects.js
@@ -7,6 +7,69 @@ Alloy.UI.Effects = {
     var a = new Animation();
     a.transform = t2;
     view.animate(a); 
+  },
+  
+  activate: function(view, onComplete) {
+    var t = Ti.UI.create2DMatrix().scale(2);
+    var a = new Animation();
+    a.transform = t;
+    a.opacity = 0;
+    a.addEventListener('complete', function() {
+      if (onComplete)
+        onComplete();
+        
+      var a2 = new Animation();
+      var t2 = Ti.UI.create2DMatrix().scale(1);
+      a2.transform = t2;
+      a2.opacity = 1;
+      a2.duration = 300;
+      setTimeout(function() {
+        view.animate(a2);       
+      }, 1000);
+    });
+    view.animate(a);     
+  },
+  
+  nod: function(view) {
+    var count = 0;
+    
+    var left = Ti.UI.create2DMatrix().rotate(-15);
+    var leftAnimation = new Animation();
+    leftAnimation.transform = left;
+    leftAnimation.duration = 200;
+    
+    var right = Ti.UI.create2DMatrix().rotate(15);
+    var rightAnimation = new Animation();
+    rightAnimation.transform = right;
+    rightAnimation.duration = 200;
+    
+    leftAnimation.addEventListener('complete', function() {
+      if (count < 10) {
+        view.animate(rightAnimation);
+        count++;
+      }
+      else {
+        var t = Ti.UI.create2DMatrix().rotate(0);
+        var anim = new Animation();
+        anim.transform = t;
+        view.animate(anim);
+      }
+    })
+
+    rightAnimation.addEventListener('complete', function() {
+      if (count < 10) {
+        view.animate(leftAnimation);
+        count++;
+      }
+      else {
+        var t = Ti.UI.create2DMatrix().rotate(0);
+        var anim = new Animation();
+        anim.transform = t;
+        view.animate(anim);
+      }
+    })
+    
+    view.animate(leftAnimation);
   }
 }
 
