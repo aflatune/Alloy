8c856efdecd0be833d0c483b1500b1f5e07784f1
Amol Kelkar <amolk@aflatune.com>
2011-11-06 00:15:53 -0700

    Image button

--- ui/image_button.js
+++ ui/image_button.js
@@ -1,35 +1,28 @@
 Alloy.ImageButton = function(params) {
-  params.height = params.height || 30;
-  if (!params.title) {
-    warn("Must specify title for ImageButton");
-  }
-  
   params.text = params.title;
-  params.width = params.width || params.title.length * 12;
-  params.font = params.font || {fontSize: 12, fontWeight: 'bold'};
-  params.textAlign = params.textAlign || 'center';
-  params.color = params.color || "#fff";
   
   var button = new Label(params);
-  $(button).applyStyle('ImageButton', params.className);
+  $(button).applyStyle('ImageButton', params);
   
   if (button.backgroundImage) {
     button.backgroundImageNormal = button.backgroundImage;
     
-    button.addEventListener('touchstart', function() {
-      info('touchstart');
-      button.backgroundImage = button.backgroundImagePressed;
-    });
-    
-    button.addEventListener('touchend', function() {
-      info('touchend');
-      button.backgroundImage = button.backgroundImageNormal;
-    });  
-
-    button.addEventListener('touchcancel', function() {
-      info('touchcancel');
-      button.backgroundImage = button.backgroundImageNormal;
-    });
+    if (button.backgroundImagePressed) {
+      button.addEventListener('touchstart', function() {
+        info('touchstart');
+        button.backgroundImage = button.backgroundImagePressed;
+      });
+      
+      button.addEventListener('touchend', function() {
+        info('touchend');
+        button.backgroundImage = button.backgroundImageNormal;
+      });  
+  
+      button.addEventListener('touchcancel', function() {
+        info('touchcancel');
+        button.backgroundImage = button.backgroundImageNormal;
+      });
+    }
   }  
   
   return button;
