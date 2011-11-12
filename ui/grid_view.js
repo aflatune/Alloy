e8700358100a62e79ee8634bc5eaba05f61da5ac
Amol Kelkar <amolk@aflatune.com>
2011-11-12 15:32:13 -0800

    Grid view cells keep visible text

--- ui/grid_view.js
+++ ui/grid_view.js
@@ -25,6 +25,21 @@ Alloy.UI.GridViewItem = new JS.Class({
       height: this.height,
       width: this.width
     });
+    
+    var _this = this;
+    this.view.addEventListener('touchstart', function(e) {
+      Alloy.UI.setColorRecursive(_this.view, '#fff');
+    });
+  
+    this.view.addEventListener('touchcancel', function(e) {
+      Alloy.UI.resetColorRecursive(_this.view);
+    });
+    
+    this.view.addEventListener('touchend', function(e) {
+      setTimeout(function() {
+        Alloy.UI.resetColorRecursive(_this.view);
+      }, 2000);
+    });
   }
 });
 
